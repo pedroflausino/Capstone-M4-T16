ef7c75f391a612b4e68d1295b3022031d4962112
Rodrigo Méro <meroo.rodrigo@gmail.com>
2022-09-12 18:13:49 -0300

    Feat: added categories routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,12 @@ import { userRoutes } from "./users.routes";
 import { companiesRoutes } from "./companies.routes";
 import { orderRoutes } from "./orders.routes";
 import { productsRoutes } from "./products.routes";
+import { categoriesRoutes } from "./categories.routes";
 
 export const appRoutes = (app: Express) => {
-  app.use("/orders", orderRoutes());
-  app.use("/companies", companiesRoutes())
-  app.use("/users", userRoutes());
-  app.use("/products", productsRoutes());
+    app.use("/orders", orderRoutes());
+    app.use("/companies", companiesRoutes());
+    app.use("/users", userRoutes());
+    app.use("/products", productsRoutes());
+    app.use("/categories", categoriesRoutes());
 };
-
